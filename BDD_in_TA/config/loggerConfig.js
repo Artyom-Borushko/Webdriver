6c5731ddb75cd011e2048a2aa3f242337254e4bf
Artyom Borushko <47773490+Artyom-Borushko@users.noreply.github.com>
2020-04-29 22:18:37 +0300

    rename log file

--- BDD_in_TA/config/loggerConfig.js
+++ BDD_in_TA/config/loggerConfig.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 const logger = winston.createLogger({
 	level: 'debug',
 	transports: [
-		new winston.transports.File({ filename: 'loggerOutput.log', level: 'debug' }),
+		new winston.transports.File({ filename: 'combined.log', level: 'debug' }),
 	],
 	format: winston.format.simple(),
 });
